d2c5fd199c633557e8d4c4f366a8f9f3c0f2c921
Kevin Wong <kevingywong@gmail.com>
2021-12-27 19:14:03 -0500

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,48 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import Axios from 'axios';
+import styled from 'styled-components';
+import {Header, AppNameComponent, AppIcon, SearchIcon, SearchComponent, SearchInput} from './components/headerComponent';
+import {RecipeListContainer, RecipeContainer, CoverImage, RecipeName, IngredientsText, SeeMoreText} from './components/recipeComponent';
+
+const APP_ID = "c9d4abe1";
+const APP_KEY = "3cc789b19047905667109f322e22dd87";
+
+const Container = styled.div `
+  display: flex;
+  flex-direction: column;
+`;
 
 function App() {
+  const [timeoutId, updateTimeoutId] = useState();
+
+  const fetchRecipe = (searchString)=> {
+    Axios.get(`{}`)
+  }
+
+  const onTextChange = (e) => {
+    clearTimeout(timeoutId);
+    const timeout = setTimeout(() => console.log("API call"), 500);
+    updateTimeoutId(timeoutId)
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Container>
+      <Header>
+        <AppNameComponent><AppIcon src="/hamburger-icon.svg" />Recipe Finder</AppNameComponent>
+        <SearchComponent>
+          <SearchIcon src="/search-icon.svg" />
+          <SearchInput placeholder="Search Recipes" onChange={onTextChange} />
+        </SearchComponent>
+      </Header>
+      <RecipeListContainer>
+      <RecipeContainer>
+        <CoverImage src="/hamburger-icon.svg" />
+        <RecipeName>Food Name</RecipeName>
+        <IngredientsText>Ingredients</IngredientsText>
+        <SeeMoreText>See Recipe</SeeMoreText>
+      </RecipeContainer>
+      </RecipeListContainer>
+    </Container>
   );
 }
 
